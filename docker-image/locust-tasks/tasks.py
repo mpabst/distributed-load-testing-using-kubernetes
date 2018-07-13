dd526c8a4e4bfb1a91d8f992b6051ef91928b2ec
Michael Pabst <michael.k.pabst@gmail.com>
2018-07-13 20:08:11 +0000

    Replacing sample locustfile with our own real one

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -1,42 +1,35 @@
-#!/usr/bin/env python
-
-# Copyright 2015 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-import uuid
-
-from datetime import datetime
 from locust import HttpLocust, TaskSet, task
+# import pydevd as pydevd
 
 
-class MetricsTaskSet(TaskSet):
-    _deviceid = None
-
-    def on_start(self):
-        self._deviceid = str(uuid.uuid4())
+class UserBehavior(TaskSet):
 
     @task(1)
-    def login(self):
-        self.client.post(
-            '/login', {"deviceid": self._deviceid})
+    def loadUser(self):
+        self.client.headers['Content-Type'] = "application/json"
+        self.client.headers['Authorization'] = "Bearer " + self.token
+        query_string = "{user(id: " + str(self.id) + "){id,firstName, lastName, photos {id, thumbnailUrl, url}}}"
+        print(query_string)
+        self.client.post("/graphql", json={"query": query_string})
 
-    @task(999)
-    def post_metrics(self):
-        self.client.post(
-            "/metrics", {"deviceid": self._deviceid, "timestamp": datetime.now()})
+    @task(1)
+    def photoSearch(self):
+        self.client.headers['Content-Type'] = "application/json"
+        self.client.headers['Authorization'] = "Bearer " + self.token
+        query_string = "{photoExploreSearch(requestingUser: " + str(self.id) + ", userOnly: false, exifStrings: [\"canon\"], industries: [100,101,102], tags: [100,101,102]) {count, id, thumbnailUrl, url}}"
+        print(query_string)
+        self.client.post("/graphql", json={"query": query_string})
+
+    def on_start(self):
+ #       pydevd.settrace('localhost', port=12345, stdoutToServer=True, stderrToServer=True)
+        self.client.headers['Content-Type'] = "application/json"
+        response = self.client.post("/graphql", json={"query": "mutation {login(email:\"rlexvold@gmail.com\", password:\"Lutef1sk\"){ access_token, userId}}"})
+        json_response_dict = response.json()
+        self.token = json_response_dict['data']['login']['access_token']
+        self.id = json_response_dict['data']['login']['userId']
 
 
-class MetricsLocust(HttpLocust):
-    task_set = MetricsTaskSet
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 9000
